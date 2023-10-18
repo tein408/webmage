ceebdc24923fe12214fab83a3c31966419f57ce6
hoi <coghd28@naver.com>
2023-10-18 16:48:50 +0900

    Modify <김채홍:settings.py>
    주석 수정

--- manda_project/manda_project/settings.py
+++ manda_project/manda_project/settings.py
@@ -41,8 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    #앱 이름
-    'manda_app',
+    'manda_app',#앱 이름
 ]
 
 MIDDLEWARE = [
