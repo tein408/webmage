740349adb787ccf530bb75a267c525105148f97a
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-10-18 20:18:26 +0900

    Add <강은하 : 회원가입 추가>

--- manda_project/manda_app/manda_views/views_users.py
+++ manda_project/manda_app/manda_views/views_users.py
@@ -1,10 +1,11 @@
-from rest_framework import status, generics
+from rest_framework import status
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from ..serializers.user_serializer import UserSerializer
 
 @csrf_exempt
@@ -25,7 +26,15 @@ def user_login(request):
     else:
         return HttpResponse(status=405)
     
-@csrf_exempt
+@api_view(['POST'])
 def user_logout(requet):
     logout(requet)
     return HttpResponse(status=200)
+
+@api_view(['POST'])
+def sign_up(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
