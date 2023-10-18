a121a6b3c3949d70f11fe363d0eb0e442b89f1f9
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-10-18 17:49:55 +0900

    Add <강은하 : login path 추가>

--- manda_project/manda_app/manda_urls/urls_user.py
+++ manda_project/manda_app/manda_urls/urls_user.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from ..manda_views import views_users
 
 urlpatterns = [
-    
+    path('login/', views_users.user_login, name='login'), 
 ]
