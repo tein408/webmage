e7770d3d5931024c2e01624abc37bb19927f3c1b
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-10-18 17:51:35 +0900

    Add <강은하 : 로그인 테스트코드 추가>

--- manda_project/manda_app/tests.py
+++ manda_project/manda_app/tests.py
@@ -1,3 +1,69 @@
-from django.test import TestCase
+import coverage
+cov = coverage.Coverage()
+cov.start()
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from django.contrib.auth.models import User
+
+class LoginAPITest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.login_url = reverse('login')
+        self.user_data = {
+            'username': 'testuser',
+            'email': 'test@example.com',
+            'password': 'testpassword'
+        }
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='test@example.com',
+            password='testpassword'
+        )
+
+    def test_login_valid_user(self):
+        response = self.client.post(self.login_url, self.user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_login_invalid_user(self):
+        invalid_data = {
+            'username': 'nottestuser',
+            'email': 'test@example.com',
+            'password': 'wrongpassword'
+        }
+        response = self.client.post(self.login_url, invalid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_login_missing_fields(self):
+        incomplete_data = {
+            'username': 'testuser',
+            'email': 'test@example.com',
+        }
+        response = self.client.post(self.login_url, incomplete_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_login_inactive_user(self):
+        self.user.is_active = False
+        self.user.save()
+        
+        response = self.client.post(self.login_url, self.user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_login_invalid_password(self):
+        invalid_password_data = {
+            'username': 'testuser',
+            'email': 'test@example.com',
+            'password': 'wrongpassword'
+        }
+        response = self.client.post(self.login_url, invalid_password_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_invalid_http_method(self):
+        response = self.client.get(self.login_url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+cov.stop()
+cov.save()
+cov.html_report()
