9c99dc77f2823e054fb10ea6d312a176881c0b1e
hoi <coghd28@naver.com>
2023-10-18 14:32:59 +0900

    Add <김채홍 : views.py>
    main 반환
    feed view 반환 추가

--- manda_project/manda_app/views.py
+++ manda_project/manda_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Feed
+
 
 # Create your views here.
+def main(request):
+    return render(request, 'main.html')
+
+def feed_list_view(request):
+    feeds = Feed.objects.all()
+    data = [{'id': feed.id, 'feed_contents': feed.feed_contents} for feed in feeds]
+    return JsonResponse(data, safe=False)
