2736f85c979dd385f5d1e0800b42ea63d2d89e25
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-10-18 20:18:15 +0900

    Add <강은하 : 회원가입 추가>

--- manda_project/manda_app/manda_urls/urls_user.py
+++ manda_project/manda_app/manda_urls/urls_user.py
@@ -5,4 +5,5 @@ from ..manda_views import views_users
 urlpatterns = [
     path('login/', views_users.user_login, name='login'), 
     path('logout/', views_users.user_logout, name='logout'), 
+    path('signup/', views_users.sign_up, name='signup'), 
 ]
