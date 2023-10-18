8410de5df3950a917fb499bfed8ff5b13dba62d6
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-10-18 20:26:54 +0900

    Modify <강은하 : 이메일 추가>

--- manda_project/manda_app/serializers/user_serializer.py
+++ manda_project/manda_app/serializers/user_serializer.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'password')
+        fields = ('username', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
