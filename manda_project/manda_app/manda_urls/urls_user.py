4c02ef8fccf33a23d02c775192cd01a71196baf6
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-10-18 20:26:27 +0900

    Add <강은하 : 회원정보 수정 추가>

--- manda_project/manda_app/manda_urls/urls_user.py
+++ manda_project/manda_app/manda_urls/urls_user.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views_users.user_login, name='login'), 
     path('logout/', views_users.user_logout, name='logout'), 
     path('signup/', views_users.sign_up, name='signup'), 
+    path('edit/', views_users.user_edit, name='edit'),
 ]
