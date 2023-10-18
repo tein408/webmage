17dfb0a41324064fbf4cf2cd039129dd4c5447e1
hoi <coghd28@naver.com>
2023-10-18 16:48:02 +0900

    Update <김채홍 : views_wirte.py>
    글쓰기 함수 생성

--- manda_project/manda_app/manda_views/views_write.py
+++ manda_project/manda_app/manda_views/views_write.py
@@ -1,18 +1,20 @@
 from django.shortcuts import redirect, render
-from manda_project.manda_app.models import UserProfile
+from ..forms import FeedForm  # 게시글 폼을 사용하기 위해 필요한 import
 
 # 글쓰기
 def write(request):
-    if request.user.is_authenticated: # 로그인이 된 유저면
-        try:
-            user_profile = UserProfile.objects.get(user=request.user)
-        
-            if user_profile.region_certification == 'Y':
-                return render(request, 'carrot_app/write.html')
-            else:
-                return redirect('alert', alert_message='동네인증이 필요합니다.')
-        except UserProfile.DoesNotExist:
-            return redirect('alert', alert_message='동네인증이 필요합니다.')
-    
-    else: # 로그인이 되지 않은 유저면 
-        return redirect('alert', alert_message = '로그인이 필요합니다.')
+    if request.method == 'POST':
+        form = FeedForm(request.POST, request.FILES)  # 게시글 폼으로부터 데이터 받기
+
+        if form.is_valid():
+            # 폼이 유효하면 데이터베이스에 게시글 저장
+            new_feed = form.save(commit=False)
+            new_feed.user = request.user  # 현재 로그인한 사용자를 게시글 작성자로 설정
+            new_feed.save()
+
+            return redirect('feed_list')  # 게시글 목록 페이지로 리다이렉트
+
+    else:
+        form = FeedForm()
+
+    return render(request, 'write.html', {'form': form})
