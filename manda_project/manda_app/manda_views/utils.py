b6b1494366b665fabf9845ea198c01ed9a40f241
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-10-19 22:06:33 +0900

    Update <강은하 : 이메일 발송시 css 추가>

--- manda_project/manda_app/manda_views/utils.py
+++ manda_project/manda_app/manda_views/utils.py
@@ -2,6 +2,7 @@ import random
 import string
 from django.core.mail import EmailMessage
 from django.contrib.auth import get_user_model
+from django.template.loader import render_to_string
 
 User = get_user_model()
 
@@ -11,10 +12,13 @@ def generate_temp_password(length=8):
     return temp_password
 
 def send_temp_password_email(user, temp_password):
-    subject = '[웹법사와 함께 만드는 만다라트] 임시비밀번호 안내'
-    message = f'입력하신 비밀번호는 암호화 되어있어 임시로 생성된 비밀번호를 안내해 드립니다.\n\n임시비밀번호: {temp_password}\n\n안내해 드리는 비밀번호로 로그인 하시기 바랍니다.'
-    from_email = 'webmage_manda@naver.com'
-    recipient_list = [user.email]
-    print(recipient_list)
+    title = '[웹법사와 함께 만드는 만다라트] 임시비밀번호 안내'
+    content = {
+        "message": temp_password,
+    }
+    receive_email = [user.email]
+    emailContent = render_to_string('email.html', content)
 
-    EmailMessage(subject=subject, body=message, to=recipient_list, from_email=from_email).send()
+    emailObject = EmailMessage(title, emailContent, to=receive_email)
+    emailObject.content_subtype = "html"
+    emailObject.send()
