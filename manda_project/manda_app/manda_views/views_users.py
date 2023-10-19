f9ab75945985f3fafcc9134b793e06772809f659
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-10-19 22:28:16 +0900

    Delete <강은하 : 사용하지 않는 코드 삭제>

--- manda_project/manda_app/manda_views/views_users.py
+++ manda_project/manda_app/manda_views/views_users.py
@@ -1,5 +1,4 @@
 from rest_framework import status
-from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
