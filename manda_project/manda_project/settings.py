f0722003f12aeb94e720339fd4f04147d4ba4bcb
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-10-19 22:10:01 +0900

    Add <강은하 : SMTP 세팅 추가>

--- manda_project/manda_project/settings.py
+++ manda_project/manda_project/settings.py
@@ -128,3 +128,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#### SMTP setting ####
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.naver.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'webmage_manda@naver.com'
+EMAIL_HOST_PASSWORD = secrets['SMTP_PASSWORD']
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
