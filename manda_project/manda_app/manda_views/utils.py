d039608e96c70f8a413edc94e808036f193a9b86
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-10-19 19:26:23 +0900

    Modify <강은하 : 임시비밀번호 발급 문구 수정>

--- manda_project/manda_app/manda_views/utils.py
+++ manda_project/manda_app/manda_views/utils.py
@@ -1,6 +1,6 @@
 import random
 import string
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -11,9 +11,10 @@ def generate_temp_password(length=8):
     return temp_password
 
 def send_temp_password_email(user, temp_password):
-    subject = 'Temporary Password'
-    message = f'Your temporary password is: {temp_password}'
-    from_email = 'noreply@example.com'  # 이메일 발신자 주소
-    recipient_list = [user.email]  # 사용자의 이메일 주소
+    subject = '[웹법사와 함께 만드는 만다라트] 임시비밀번호 안내'
+    message = f'입력하신 비밀번호는 암호화 되어있어 임시로 생성된 비밀번호를 안내해 드립니다.\n\n임시비밀번호: {temp_password}\n\n안내해 드리는 비밀번호로 로그인 하시기 바랍니다.'
+    from_email = 'webmage_manda@naver.com'
+    recipient_list = [user.email]
+    print(recipient_list)
 
-    send_mail(subject, message, from_email, recipient_list, fail_silently=False)
+    EmailMessage(subject=subject, body=message, to=recipient_list, from_email=from_email).send()
