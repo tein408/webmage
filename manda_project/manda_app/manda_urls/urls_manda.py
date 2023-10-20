47e631271a3b1d32098d8abbe0302a1499cc0750
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-10-20 18:50:35 +0900

    Add <강은하 : 만다라트 수정 추가>

--- manda_project/manda_app/manda_urls/urls_manda.py
+++ manda_project/manda_app/manda_urls/urls_manda.py
@@ -3,4 +3,5 @@ from ..manda_views import views_mandas
 
 urlpatterns = [
     path('create/', views_mandas.manda_main_create, name='create'), 
+    path('edit/sub/', views_mandas.update_manda_subs, name='edit_sub'),
 ]
