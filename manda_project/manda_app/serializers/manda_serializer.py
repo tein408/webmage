c652cbf3e6ca68e7e8cf05d89f632ccd285db338
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-10-20 18:50:59 +0900

    Add <강은하 : 만다라트 수정 추가>

--- manda_project/manda_app/serializers/manda_serializer.py
+++ manda_project/manda_app/serializers/manda_serializer.py
@@ -4,9 +4,30 @@ from ..models import MandaMain, MandaSub, MandaContent
 class MandaMainSerializer(serializers.ModelSerializer):
     class Meta:
         model = MandaMain
-        fields = ('user', 'success', 'main_title')
+        fields = ('id', 'user', 'main_title')
 
     def validate_main_title(self, value):
-        if len(value) > 100:
-            raise serializers.ValidationError("title은 100자 이하여야 합니다.")
+        if len(value) > 30:
+            raise serializers.ValidationError("title은 30자 이하여야 합니다.")
         return value
+
+class MandaSubUpdateSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    sub_title = serializers.CharField()
+
+    def validate_sub_title(self, value):
+        if len(value) > 50:
+            raise serializers.ValidationError("세부 목표는 50글자 이하여야 합니다.")
+        return value
+
+manda_sub_update_schema = {
+    "type": "array",
+    "items": {
+        "type": "object",
+        "properties": {
+            "id": {"type": "integer"},
+            "sub_title": {"type": "string"}
+        },
+        "required": ["id", "sub_title"]
+    }
+}
