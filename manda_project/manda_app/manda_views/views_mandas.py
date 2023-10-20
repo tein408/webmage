4b1dafcfb62c1aa25d9ee9f186db0dedcb283e56
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-10-20 12:53:26 +0900

    Modify <강은하 : 시리얼라이저 사용하도록 수정>

--- manda_project/manda_app/manda_views/views_mandas.py
+++ manda_project/manda_app/manda_views/views_mandas.py
@@ -1,29 +1,22 @@
 from rest_framework import status
 from django.contrib.auth.models import User
-from ..models import MandaMain, MandaSub, MandaContent
 from rest_framework.response import Response
 from django.http import HttpResponse, JsonResponse
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from ..models import MandaMain, MandaSub, MandaContent
+from ..serializers.manda_serializer import MandaMainSerializer
 
 from drf_yasg.utils import swagger_auto_schema
 
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def manda_main_create(request):
-    if request.user.is_authenticated:
-        user = User.objects.get(pk=request.user.id)
-
-        try:
-            title = request.data['title']
-
-            if len(title) > 100:
-                return Response({'message': 'Title is too long!'}, status=status.HTTP_400_BAD_REQUEST)
-            elif len(title) == 0 or title == '':
-                return Response({'message': 'Please write title.'}, status=status.HTTP_400_BAD_REQUEST)
-            
-            MandaMain.objects.create(user=user, main_title=title, success=False)
-        except KeyError:
-            return Response({'message': 'Please write title.'}, status=status.HTTP_400_BAD_REQUEST)
-
-        return HttpResponse(status=200)
-    else:
-        return HttpResponse(status=401)
+    user = request.user
+    serializer = MandaMainSerializer(data=request.data)
+    
+    if serializer.is_valid():
+        serializer.save(user=user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
