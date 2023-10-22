077b424a103f99183ffbf31eb7b0e84d97faaa70
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-10-22 19:00:35 +0900

    Add <강은하 : 만다라트 삭제, 내 만다라트 조회 추가>

--- manda_project/manda_app/manda_urls/urls_manda.py
+++ manda_project/manda_app/manda_urls/urls_manda.py
@@ -4,4 +4,6 @@ from ..manda_views import views_mandas
 urlpatterns = [
     path('create/', views_mandas.manda_main_create, name='create'), 
     path('edit/sub/', views_mandas.update_manda_subs, name='edit_sub'),
+    path('delete/<int:manda_id>', views_mandas.manda_main_delete, name='delete_manda'),
+    path('mandamains/<int:manda_id>', views_mandas.manda_main_list, name='mandamains'),
 ]
