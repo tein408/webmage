8791c4eeadcfb4544e9a500c223b6086ddd8e383
hoi <coghd28@naver.com>
2023-10-23 09:15:28 +0900

    Update <김채홍 : urls.py>

--- manda_project/manda_app/urls.py
+++ manda_project/manda_app/urls.py
@@ -2,13 +2,16 @@ from django.urls import path, include
 from . import views
 from django.contrib import admin
 from django.conf.urls.static import static
+from django.urls import path
+from manda_app.views import TestView
 
 from .manda_urls.urls_user import urlpatterns as manda_user_urls
-from .manda_urls.urls_write import urlpatterns as manda_write_urls
+from .manda_urls.urls_write import urlpatterns as manda_app_write_urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('v1/test/', TestView.as_view(), name='test'),
     path('', views.main, name='main'),
+    path('admin/', admin.site.urls),
     path('user/', include(manda_user_urls)), #회원가입, 로그인, 로그아웃 url
-    path('write/', include(manda_write_urls)), #글 작성, 글 선택
+    path('write/', include(manda_app_write_urls)), #글 작성, 글 선택
 ]
--- manda_project/manda_project/settings.py
+++ manda_project/manda_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'manda_app',#앱 이름
 ]
 
