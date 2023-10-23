1ca13de0c26cb18006b3d4f8e28e674025023662
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-10-23 10:02:30 +0900

    Add <강은하 : 만다라트 url 추가>

--- manda_project/manda_app/urls.py
+++ manda_project/manda_app/urls.py
@@ -5,10 +5,12 @@ from django.conf.urls.static import static
 
 from .manda_urls.urls_user import urlpatterns as manda_user_urls
 from .manda_urls.urls_write import urlpatterns as manda_write_urls
+from .manda_urls.urls_manda import urlpatterns as manda_manda_urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.main, name='main'),
     path('user/', include(manda_user_urls)), #회원가입, 로그인, 로그아웃 url
     path('write/', include(manda_write_urls)), #글 작성, 글 선택
+    path('manda/', include(manda_manda_urls)), #만다라트
+    path('get_token/', views.get_csrf_token, name='get_token'),
 ]
