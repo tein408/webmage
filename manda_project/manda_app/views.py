0ea865e67363c41fb9f84f86d13c30008d82e5b9
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-10-23 10:02:58 +0900

    Add <강은하 : getToken 추가>

--- manda_project/manda_app/views.py
+++ manda_project/manda_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Feed
+from django.middleware.csrf import get_token
+from rest_framework.decorators import api_view
 
 
 # Create your views here.
@@ -11,3 +13,8 @@ def feed_list_view(request):
     feeds = Feed.objects.all()
     data = [{'id': feed.id, 'feed_contents': feed.feed_contents} for feed in feeds]
     return JsonResponse(data, safe=False)
+
+@api_view(['GET'])
+def get_csrf_token(request):
+    csrf_token = get_token(request)
+    return JsonResponse({'csrf_token': csrf_token})
