e14f07554a6f91e32cb75d07e5c8e2a3dc690e29
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-10-25 15:47:33 +0900

    Add <강은하 : chat history 추가>

--- manda_project/manda_app/manda_urls/urls_chat.py
+++ manda_project/manda_app/manda_urls/urls_chat.py
@@ -3,4 +3,5 @@ from ..manda_views import views_chat
 
 urlpatterns = [
     path('rooms/', views_chat.get_rooms, name='rooms'), 
+    path('current/<int:room_number>/<int:sender_id>', views_chat.chat_history, name='current'), 
 ]
