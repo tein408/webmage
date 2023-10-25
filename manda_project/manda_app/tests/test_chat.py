72b5195f4244f55d55cea24e85f86eca85e1dbea
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-10-25 14:24:15 +0900

    Modify <강은하 : chat 테스트코드 수정>

--- manda_project/manda_app/tests/test_chat.py
+++ manda_project/manda_app/tests/test_chat.py
@@ -9,23 +9,38 @@ from ..models import ChatRoom, ChatMessage, UserProfile
 class GetRoomsTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.user2 = User.objects.create_user(username='testuser2', password='testpassword')
         self.client.login(username='testuser', password='testpassword')
         self.url = reverse('rooms')
 
     def test_get_rooms(self):
         chat_room1 = ChatRoom.objects.create(starter=self.user)
-        chat_room2 = ChatRoom.objects.create(receiver=self.user)
-        ChatMessage.objects.create(chat_room=chat_room1, content='Hello', author=self.user)
-        ChatMessage.objects.create(chat_room=chat_room2, content='Hi', author=self.user)
+        chat_room2 = ChatRoom.objects.create(starter=self.user2, receiver=self.user)
 
-        response = self.client.post(self.url, format='json')
+        ChatMessage.objects.create(chatroom=chat_room1, content='Hello', author=self.user)
+        ChatMessage.objects.create(chatroom=chat_room2, content='Hi', author=self.user2)
 
-        rooms_data = response.data
-        first_room_data = rooms_data[0]
-        self.assertEqual(response.status_code, 200)
+        response = self.client.get(self.url)
+        rooms_data = response.data.get('rooms')
+
+        # 내가 속한 채팅방 수 확인
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsNotNone(rooms_data)
         self.assertEqual(len(rooms_data), 2)
+
+        # 로그인 한 유저가 starter 인 경우
+        first_room_data = rooms_data[0]
         self.assertEqual(first_room_data['chat_room_id'], chat_room1.pk)
         self.assertEqual(first_room_data['starter_id'], self.user.pk)
         self.assertEqual(first_room_data['starter'], self.user.username)
         self.assertEqual(first_room_data['message'], 'Hello')
+        self.assertEqual(first_room_data['unread_message_count'], 0) 
+
+        # 로그인 한 유저에게 채팅이 온 경우
+        second_room_data = rooms_data[1]
+        self.assertEqual(second_room_data['chat_room_id'], chat_room2.pk)
+        self.assertEqual(second_room_data['starter_id'], self.user.pk)
+        self.assertEqual(second_room_data['starter'], self.user.username)
+        self.assertEqual(second_room_data['message'], 'Hi')
+        self.assertEqual(second_room_data['unread_message_count'], 1)
 
