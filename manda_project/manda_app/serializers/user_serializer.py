f9946f99f495e40397d0ea129f7a3e5df7ee3e1d
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-11-01 14:42:47 +0900

    Add <강은하 : 유저 프로필 보기 추가>

--- manda_project/manda_app/serializers/user_serializer.py
+++ manda_project/manda_app/serializers/user_serializer.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from ..models import UserProfile
+from django.core.validators import MaxLengthValidator
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +12,25 @@ class UserSerializer(serializers.ModelSerializer):
 class UserAuthenticationSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
+
+def validate_max_length(value):
+    max_length = 50
+    if len(value) > max_length:
+        raise serializers.ValidationError(f'{max_length} characters long.')
+    return value
+
+def validate_max_length2(value):
+    max_length = 200
+    if len(value) > max_length:
+        raise serializers.ValidationError(f'{max_length} characters long.')
+    return value
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    user_img = serializers.ImageField(max_length=None, use_url=True, write_only=True)
+    user_position = serializers.CharField(validators=[validate_max_length])
+    user_info = serializers.CharField(validators=[validate_max_length2])
+    user_hash = serializers.CharField(validators=[validate_max_length])
+
+    class Meta:
+        model = UserProfile
+        fields = ('user', 'user_img', 'user_position', 'user_info', 'user_hash', 'success_count')
