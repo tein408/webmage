374df8c1fd0d37ecd32cc914814c4d0376e55010
AdamKim <4_2@naver.com>
2023-11-01 17:53:18 +0900

    init doun

--- manda_project/manda_app/urls.py
+++ manda_project/manda_app/urls.py
@@ -8,6 +8,7 @@ from manda_app.views import TestView
 from .manda_urls.urls_user import urlpatterns as manda_user_urls
 from .manda_urls.urls_write import urlpatterns as manda_write_urls
 from .manda_urls.urls_manda import urlpatterns as manda_manda_urls
+from .manda_urls.urls_feed import urlpatterns as manda_feed_urls
 from .manda_urls.urls_chat import urlpatterns as manda_chat_urls
 
 urlpatterns = [
@@ -16,6 +17,7 @@ urlpatterns = [
     path('user/', include(manda_user_urls)), #회원가입, 로그인, 로그아웃
     path('write/', include(manda_write_urls)), #글 작성, 글 선택
     path('manda/', include(manda_manda_urls)), #만다라트
+	path('feed/', include(manda_feed_urls)), #피드
     path('chat/', include(manda_chat_urls)), #채팅
     path('get_token/', views.get_csrf_token, name='get_token'), #토큰
 ]
